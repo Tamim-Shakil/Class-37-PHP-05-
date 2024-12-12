8cefff9ddc17e4595b0a33e740ec5648a08f2840
Tamim-Shakil <tamim720sk@gmail.com>
2024-12-13 00:00:38 +0600

    Done

---
+++ L11 - loops.php
@@ -0,0 +1,31 @@
+<?php
+// start point
+// increment
+// end point
+
+// while loop
+$startPoint = 0;
+$endPoint = 9;
+
+while ($startPoint <= $endPoint) {
+    echo $startPoint . "<br>";
+    $startPoint = $startPoint + 1;
+}
+
+echo "<br>";
+
+
+// for loop
+for ($i = 0; $i <= 9; $i++) {
+    echo $i . "<br>";
+}
+
+echo "<br>";
+
+
+// do while loop
+$x = 20;
+do {
+    echo $x . "<br>";
+    $x++;
+} while ($x <= 30);
